83a745e8a4f894a9ab607c22c359d3c1a3be9569
ibaragozz <baragozz1@gmail.com>
2024-11-30 20:58:10 +0300

    lesson stack done

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
-class Stack:
-    def __init__(self):
-        self.items = []
+# class Stack:
+#     def __init__(self):
+#         self.items = []
+#
+#     def push(self, item):
+#         self.items.append(item)
+#
+#     def pop(self):
+#         return self.items.pop()
+#
+#     def is_empty(self):
+#         return self.items == []
+#
+#     def peek(self):
+#         return self.items[-1]
+#
+# stack = Stack()
+# stack.push(1)
+# stack.push(2)
+# stack.push(3)
+# print(stack.is_empty())
+# print(stack.peek())
 
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def is_empty(self):
-        return self.items == []
-
-    def peek(self):
-        return self.items[-1]
