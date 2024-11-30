000f30d6c1df051c290f33615f281f154ed92942
ibaragozz <baragozz1@gmail.com>
2024-11-30 21:05:21 +0300

    lesson qeueu done

--- main.py
+++ main.py
@@ -21,3 +21,21 @@
 # print(stack.is_empty())
 # print(stack.peek())
 
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
