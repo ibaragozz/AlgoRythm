6fc0ea85fe3e28f9d384f0b3a66fd019631dd3a4
ibaragozz <baragozz1@gmail.com>
2024-11-30 21:35:22 +0300

    HWdone

--- main.py
+++ main.py
@@ -1,59 +1,59 @@
-# class Stack:
-#     def __init__(self):
-#         self.items = []
-#
-#     def push(self, item):
-#         self.items.append(item)
-#
-#     def pop(self):
-#         return self.items.pop()
-#
-#     def is_empty(self):
-#         return self.items == []
-#
-#     def peek(self):
-#         return self.items[-1]
-#
-# stack = Stack()
-# stack.push(1)
-# stack.push(2)
-# stack.push(3)
-# print(stack.is_empty())
-# print(stack.peek())
+class Stack:
+    def __init__(self):
+        self.items = []
 
-# class Queue:
-#     def __init__(self):
-#         self.items = []
-#
-#     def enqueue(self, item):
-#         self.items.insert(0, item)
-#
-#     def dequeue(self):
-#         return self.items.pop()
-#
-#     def is_empty(self):
-#         return self.items == []
-#
-#     def peek(self):
-#         return self.items[-1]
-#
-#     def size(self):
-#         return len(self.items)
-#
-#
-# queue = Queue()
-# print(queue.is_empty())
-#
-# queue.enqueue("Act 1")
-# queue.enqueue("Act 2")
-# queue.enqueue("Act 3")
-# queue.enqueue("Act 4")
-#
-# print(queue.is_empty())
-# print(queue.size())
-#
-# print(queue.dequeue())
-# print(queue.size())
+    def push(self, item):
+        self.items.append(item)
+
+    def pop(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        return self.items[-1]
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+print(stack.is_empty())
+print(stack.peek())
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self, item):
+        self.items.insert(0, item)
+
+    def dequeue(self):
+        return self.items.pop()
+
+    def is_empty(self):
+        return self.items == []
+
+    def peek(self):
+        return self.items[-1]
+
+    def size(self):
+        return len(self.items)
+
+
+queue = Queue()
+print(queue.is_empty())
+
+queue.enqueue("Act 1")
+queue.enqueue("Act 2")
+queue.enqueue("Act 3")
+queue.enqueue("Act 4")
+
+print(queue.is_empty())
+print(queue.size())
+
+print(queue.dequeue())
+print(queue.size())
 
 class Node:
     def __init__(self, key):
@@ -81,3 +81,26 @@ root = insert(root, 60)
 root = insert(root, 73)
 root = insert(root, 98)
 root = insert(root, 84)
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        self.graph[u].append(v)
+
+    def print_graph(self):
+        for node in self.graph:
+            print(node, "->", ' -> '.join(map(str, self.graph[node])))
+
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 4)
+g.add_edge(1, 2)
+g.add_edge(1, 3)
+g.add_edge(1, 4)
+g.add_edge(2, 3)
+g.add_edge(3, 4)
+g.print_graph()
