99b515838bcd3fdfa6031f06a1a2f2290ea41899
ibaragozz <baragozz1@gmail.com>
2024-12-01 10:03:32 +0300

    bubble sort done

--- sorting.py
+++ sorting.py
@@ -1,1 +1,8 @@
-mas = [5,6,2,3,7,1]
+mas = [5, 6, 2, 3, 7, 1]
+n = 6
+
+for run in range(n-1):
+    for i in range(n-1-run):
+        if mas[i] > mas[i + 1]:
+            mas[i], mas[i + 1] = mas[i + 1], mas[i]
+print(mas)
